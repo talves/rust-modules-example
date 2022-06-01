57d1be8bf4d33625d30073ac0dc7a2e41ab8c3b3
talves <tony@3alves.com>
2022-06-01 10:20:51 -0700

    pre module setup for brevity

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,21 @@ fn connect_to_database() -> Status {
     return Status::Connected;
 }
 
-fn authenticate(creds: Credentials) {}
+fn get_user() {
+    //get user from db
+}
+
+fn login(creds: Credentials) {
+    // authenticate...
+    get_user();
+}
+
+fn logout() {
+    //logout the user...
+}
+
+fn authenticate(creds: Credentials) {
+    if let Status::Connected = connect_to_database() {
+        login(creds);
+    }
+}
