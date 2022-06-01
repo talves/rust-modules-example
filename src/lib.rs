ddf768cd8ecd74acc77843ab26e15fb2f777daf7
talves <tony@3alves.com>
2022-06-01 13:23:45 -0700

    use single file module format

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
 #![allow(dead_code, unused_variables)]
-mod database {
-
-    pub enum Status {
-        Connected,
-        Interrupted,
-    }
-
-    pub fn connect_to_database() -> Status {
-        return Status::Connected;
-    }
-
-    pub fn get_user() {
-        //get user from db
-    }
-}
+mod database;
 
 mod auth_utils {
 
