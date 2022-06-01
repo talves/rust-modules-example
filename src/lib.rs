ef784b147f50f66c31642bcaa39df1c5b4009c2d
talves <tony@3alves.com>
2022-06-01 13:07:42 -0700

    convert into using modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,41 @@
 #![allow(dead_code, unused_variables)]
-pub struct Credentials {
-    username: String,
-    password: String,
-}
-enum Status {
-    Connected,
-    Interrupted,
+mod database {
+
+    pub enum Status {
+        Connected,
+        Interrupted,
+    }
+
+    pub fn connect_to_database() -> Status {
+        return Status::Connected;
+    }
+
+    pub fn get_user() {
+        //get user from db
+    }
 }
 
-fn connect_to_database() -> Status {
-    return Status::Connected;
-}
+mod auth_utils {
 
-fn get_user() {
-    //get user from db
-}
+    pub fn login(creds: models::Credentials) {
+        // authenticate...
+        crate::database::get_user();
+    }
 
-fn login(creds: Credentials) {
-    // authenticate...
-    get_user();
-}
+    pub fn logout() {
+        //logout the user...
+    }
 
-fn logout() {
-    //logout the user...
-}
+    mod models {
+        pub struct Credentials {
+            username: String,
+            password: String,
+        }
+    }
 
-pub fn authenticate(creds: Credentials) {
-    if let Status::Connected = connect_to_database() {
-        login(creds);
+    pub fn authenticate(creds: models::Credentials) {
+        if let crate::database::Status::Connected = crate::database::connect_to_database() {
+            login(creds);
+        }
     }
 }
