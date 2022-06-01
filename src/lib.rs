8fd9a5c6ed267e02b388cf7749e84a67fb169c8a
talves <tony@3alves.com>
2022-06-01 10:15:40 -0700

    db connect function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+#![allow(dead_code, unused_variables)]
 struct Credentials {
     username: String,
     password: String,
 }
+enum Status {
+    Connected,
+    Interrupted,
+}
+
+fn connect_to_database() -> Status {
+    return Status::Connected;
+}
+
 fn authenticate(creds: Credentials) {}
